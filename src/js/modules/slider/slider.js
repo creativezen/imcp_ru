99bafc69d7b41b9b76c0c632b713274a9a3f5a1e
Anton Goul'art <tocha2782@gmail.com>
2024-10-10 18:25:29 +0300

    fixed bugs in swiper js

--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -8,58 +8,42 @@ export function init() {
 
     let sliderItems
     let sliderCentered
-    let sliderInit
     let sliderLoop
 
 	if (slidersArray.length) {
+
 		slidersArray.forEach(slider => {
+            
             let sliderName = slider.dataset.sliderName
 			let sliderSelector = `[data-slider-name="${sliderName}"]`
 
-            // document.querySelector(`${sliderSelector}`).dataset.sliderCenter
-            //     ? sliderCentered = true
-            //     : sliderCentered = false
-            
-            // document.querySelector(`${sliderSelector}`).dataset.sliderInit
-            //     ? sliderInit = Number(document.querySelector(`${sliderSelector}`).dataset.sliderInit)
-            //     : 1
+            document.querySelector(`${sliderSelector}`).dataset.sliderCenter
+                ? sliderCentered = true
+                : sliderCentered = false
 
-            // document.querySelector(`${sliderSelector}`).dataset.sliderLoop
-            //     ? sliderLoop = true
-            //     : sliderLoop = false
+            document.querySelector(`${sliderSelector}`).dataset.sliderLoop
+                ? sliderLoop = true
+                : sliderLoop = false
 
-			sliderItems = document.querySelector(`${sliderSelector}`).dataset.sliderItems
-
-			// Если кол-во просматриваемых слайдов больше 1, то выводим указанное кол-во, а иначе 'auto'
-			let numPerView = sliderItems > 1 || sliderItems == 1 ? sliderItems : 'auto'
+			document.querySelector(`${sliderSelector}`).dataset.sliderItems
+                ? sliderItems = Number(document.querySelector(`${sliderSelector}`).dataset.sliderItems)
+                : sliderItems = "auto"
 
 			new Swiper(sliderSelector, {
-				// freeMode: true,
+				slidesPerView: sliderItems,
+                centeredSlides: sliderCentered,
+                loop: sliderLoop,
 				spaceBetween: 10,
-				observer: true,
-				observeParents: true,
+
 				navigation: {
 					nextEl: `.slider-btn.next-${sliderName}`,
 					prevEl: `.slider-btn.prev-${sliderName}`
 				},
 
-                // centeredSlides: sliderCentered ? true : false,
-                // initialSlide: sliderInit ? sliderInit : 1,
-                // loop: sliderLoop ? true : false,
-				
 				breakpoints: {
-					680: {
-						slidesPerView: numPerView > 1 ? 2 : 'auto',
-						spaceBetween: 20,
-					},
 					1080: {
-						slidesPerView: numPerView > 1 ? 3 : 'auto',
 						spaceBetween: 20,
 					},
-					1280: {
-						slidesPerView: numPerView,
-						spaceBetween: 20,
-					}
 				}
 			})
 		})
